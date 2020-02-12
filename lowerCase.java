a676c77941f721394ac881c9e2f9f4e5830f149f
anges-bai <45257069+anges-bai@users.noreply.github.com>
2020-02-12 17:27:08 +0800

    Create lowerCase.java

---
+++ lowerCase.java
@@ -0,0 +1,9 @@
+public class acc{
+    public static boolean isAdmin(String userId){
+        return userId.toLowerCase()=="admin";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAdmin("Admin"));
+    }
+}
